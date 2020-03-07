7b029822da9766dd5f4d6cea76eaa7c3150a483c
Administrator <Administrator@3OXUO32YTY16YVA>
2020-03-07 14:51:28 +0800

    测试

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,9 +1,8 @@
 package test;
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println("hahahhaha");
+private void syso() {
+	// TODO Auto-generated method stub
+
 }
 }
-//12121212121211
-///dfsafudgsfudsg
